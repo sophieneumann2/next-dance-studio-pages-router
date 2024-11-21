41f30b2c0c6f433e5b812b0ceb0d587359ae90a2
Sophie Neumann <sophieneumann@Air-von-Sophie.fritz.box>
2024-11-21 16:04:34 +0100

    feat: tried out lodash

--- lib/courses.ts
+++ lib/courses.ts
@@ -1,7 +1,34 @@
+import _ from 'lodash';
 import todaysCoursesDummy from '../data/todays_courses.json';
 
 const DUMMY_TODAYS_COURSES = todaysCoursesDummy;
 
+/* 
+
+FAKE API RESPONSE:
+
+- id: not unique
+- course name: double, not defined
+- time: double, not defined, wrong type
+
+GOAL DATA STRUCTURE:
+
+- id: unique
+- course name: one per entry, defined & not empty
+- time: one per entry, type string, defined & not empty
+
+*/
+
 export const getTodaysClasses = () => {
-  return DUMMY_TODAYS_COURSES;
+  const structuredDataResponse = _.chain(DUMMY_TODAYS_COURSES)
+    .filter((course) => _.has(course, 'id'))
+    .filter(
+      (course) => _.has(course, 'course_name') && course.course_name.length > 0,
+    )
+    .filter(
+      (course) => _.has(course, 'time') && typeof course.time === 'string',
+    )
+    .uniqBy('id')
+    .value();
+  return structuredDataResponse;
 };
